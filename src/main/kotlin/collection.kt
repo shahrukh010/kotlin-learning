019531af24abffe3ca09098b1dbd8f2afd36965b
shahrukh <khanshahrukhbsc@gmail.com>
2023-10-31 13:55:44 +0530

    zip both collection on same index

--- src/main/kotlin/collection.kt
+++ src/main/kotlin/collection.kt
@@ -10,9 +10,10 @@ fun main(args: Array<String>){
 
 	for(user:User in userList){
 		println(user);
-	}
 
+	}
 	//set example
+	
 	
 	var user3 = User(3333,"bridget");
 	var user4 = User(4444,"nic");
@@ -25,7 +26,32 @@ fun main(args: Array<String>){
 		println(user);
 	}
 
+	val maps = mutableMapOf(1 to "one",2 to "two", 3 to "three");
+//	println(maps.keys);
+
+
+	val numbersMap = mutableMapOf("key1" to 1, "key2" to 2, "key3" to 3, "key11" to 11);
+
+	var result = numbersMap.filter{(key,value) -> key.endsWith("1") && value > 10};
+	println(result);
+	numbersMap.putAll(setOf("four" to 4,"five" to 5));
+	numbersMap.put("six",6);
+	println(numbersMap);
+
+
+	//transformation.
 	
+	println("Transformation");
+	val numbers = setOf(1,2,3);
+	println(numbers.map{it * 3});
+
+	//zip
+	println("ZIP");
+
+	val color = listOf("red","brown","yellow");
+	val animal = listOf("fox","cow","dog");
+	println(color zip animal);
+
 }
 
 data class User(var id:Int,var name:String)
